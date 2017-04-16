4146566c940c28912b3ef18614e61f9242176c74
algam <algamonline@gmail.com>
2017-04-16 21:01:47 +0700

    meta

--- application/modules/home/controllers/home.php
+++ application/modules/home/controllers/home.php
@@ -20,6 +20,13 @@ class home extends DC_controller {
 		$data = $this->controller_attr;
 		$data['function']='home';
 		$data['page'] = $this->load->view('home/index',$data,true);
+
+		 $data = array(
+			 'title' => 'Rumah Sentul',
+			 'description' => 'Jual Rumah di sentul',
+			 'keywords' => 'rumah,dijual'
+		 );
+
 		$this->load->view('layout_frontend',$data);
 	}
 	
--- application/views/layout_frontend.php
+++ application/views/layout_frontend.php
@@ -3,10 +3,34 @@
 <head>
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <meta charset="utf-8" />
-<title><?php echo $controller_name; echo $method; ?> | Sentul</title>
+<title><?php echo $title?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-<meta content="" name="description" />
-<meta content="" name="author" />
+    <meta name='robots' content='noindex, nofollow' />
+    <meta content="" name="title" />
+    <meta content="" name="author" />
+    <meta name="language" content="indonesia">
+
+    <!-- for Google -->
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+
+    <meta name="author" content="" />
+    <meta name="copyright" content="" />
+    <meta name="application-name" content="" />
+
+    <!-- for Facebook -->
+    <meta property="og:title" content="" />
+    <meta property="og:type" content="article" />
+    <meta property="og:image" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:description" content="" />
+
+    <!-- for Twitter -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:description" content="" />
+    <meta name="twitter:image" content="" />
+
 
 </head>
 
