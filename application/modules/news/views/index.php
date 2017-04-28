34358d91a4c0da650397a348d849c087bf635c74
ilhamudzakir <ilhamudzakir@gmail.com>
2017-04-28 17:24:35 +0700

    a
    a

--- application/modules/news/views/index.php
+++ application/modules/news/views/index.php
@@ -2,9 +2,9 @@
 <section class="p-xlg">
 					<div class="container">
 						<div class="row">
-						<div class="col-md-8 left-bar">
+						<div class="col-md-8 left-bar content-news">
 						<div class="title-static">
-							<h3>Berita Properti</h3>
+							<h4>Berita Properti</h4>
 						</div>
 
 						<div class="content">
@@ -14,9 +14,124 @@
 								<img src="http://localhost/rumah_sentul/assets/uploads/news/news.jpg" width="100%">
 							</div>
 							<div class="col-md-8">
-								<div><h3>Lorem ipsum dolor amet</h3></div>
+								<div class="title-news"><h5>Lorem ipsum dolor amet</h5></div>
+								<div class="posted-on"><span>Posted on : 1 Jan 2010</span></div>
+								<div>Euismod rutrum viverra a vel ultricies justo scelerisque varius a mi arcu posuere congue tellus mi a volutpat vestibulum dui a montes ullamcorper iaculis in. Risus nascetur ac in donec mattis consectetur erat eu ad parturient himenaeos dictum parturient parturient lacinia parturient sociosqu nascetur cras suspendisse inceptos...</div>
+								<br>
+								<div><a class="mt-md" href="demo-restaurant-press-detail.html">Read More <i class="fa fa-long-arrow-right"></i></a></div>
 							</div>
 						</div>
+						</section>
+
+												<section class="news-row border-bot">
+						<div class="row">
+							<div class="col-md-4">
+								<img src="http://localhost/rumah_sentul/assets/uploads/news/news.jpg" width="100%">
+							</div>
+							<div class="col-md-8">
+								<div class="title-news"><h5>Lorem ipsum dolor amet</h5></div>
+								<div class="posted-on"><span>Posted on : 1 Jan 2010</span></div>
+								<div>Euismod rutrum viverra a vel ultricies justo scelerisque varius a mi arcu posuere congue tellus mi a volutpat vestibulum dui a montes ullamcorper iaculis in. Risus nascetur ac in donec mattis consectetur erat eu ad parturient himenaeos dictum parturient parturient lacinia parturient sociosqu nascetur cras suspendisse inceptos...</div>
+								<br>
+								<div><a class="mt-md" href="demo-restaurant-press-detail.html">Read More <i class="fa fa-long-arrow-right"></i></a></div>
+							</div>
+						</div>
+						</section>
+
+												<section class="news-row border-bot">
+						<div class="row">
+							<div class="col-md-4">
+								<img src="http://localhost/rumah_sentul/assets/uploads/news/news.jpg" width="100%">
+							</div>
+							<div class="col-md-8">
+								<div class="title-news"><h5>Lorem ipsum dolor amet</h5></div>
+								<div class="posted-on"><span>Posted on : 1 Jan 2010</span></div>
+								<div>Euismod rutrum viverra a vel ultricies justo scelerisque varius a mi arcu posuere congue tellus mi a volutpat vestibulum dui a montes ullamcorper iaculis in. Risus nascetur ac in donec mattis consectetur erat eu ad parturient himenaeos dictum parturient parturient lacinia parturient sociosqu nascetur cras suspendisse inceptos...</div>
+								<br>
+								<div><a class="mt-md" href="demo-restaurant-press-detail.html">Read More <i class="fa fa-long-arrow-right"></i></a></div>
+							</div>
+						</div>
+						</section>
+
+												<section class="news-row border-bot">
+						<div class="row">
+							<div class="col-md-4">
+								<img src="http://localhost/rumah_sentul/assets/uploads/news/news.jpg" width="100%">
+							</div>
+							<div class="col-md-8">
+								<div class="title-news"><h5>Lorem ipsum dolor amet</h5></div>
+								<div class="posted-on"><span>Posted on : 1 Jan 2010</span></div>
+								<div>Euismod rutrum viverra a vel ultricies justo scelerisque varius a mi arcu posuere congue tellus mi a volutpat vestibulum dui a montes ullamcorper iaculis in. Risus nascetur ac in donec mattis consectetur erat eu ad parturient himenaeos dictum parturient parturient lacinia parturient sociosqu nascetur cras suspendisse inceptos...</div>
+								<br>
+								<div><a class="mt-md" href="demo-restaurant-press-detail.html">Read More <i class="fa fa-long-arrow-right"></i></a></div>
+							</div>
+						</div>
+						</section>
+
+												<section class="news-row border-bot">
+						<div class="row">
+							<div class="col-md-4">
+								<img src="http://localhost/rumah_sentul/assets/uploads/news/news.jpg" width="100%">
+							</div>
+							<div class="col-md-8">
+								<div class="title-news"><h5>Lorem ipsum dolor amet</h5></div>
+								<div class="posted-on"><span>Posted on : 1 Jan 2010</span></div>
+								<div>Euismod rutrum viverra a vel ultricies justo scelerisque varius a mi arcu posuere congue tellus mi a volutpat vestibulum dui a montes ullamcorper iaculis in. Risus nascetur ac in donec mattis consectetur erat eu ad parturient himenaeos dictum parturient parturient lacinia parturient sociosqu nascetur cras suspendisse inceptos...</div>
+								<br>
+								<div><a class="mt-md" href="demo-restaurant-press-detail.html">Read More <i class="fa fa-long-arrow-right"></i></a></div>
+							</div>
+						</div>
+						</section>
+
+												<section class="news-row border-bot">
+						<div class="row">
+							<div class="col-md-4">
+								<img src="http://localhost/rumah_sentul/assets/uploads/news/news.jpg" width="100%">
+							</div>
+							<div class="col-md-8">
+								<div class="title-news"><h5>Lorem ipsum dolor amet</h5></div>
+								<div class="posted-on"><span>Posted on : 1 Jan 2010</span></div>
+								<div>Euismod rutrum viverra a vel ultricies justo scelerisque varius a mi arcu posuere congue tellus mi a volutpat vestibulum dui a montes ullamcorper iaculis in. Risus nascetur ac in donec mattis consectetur erat eu ad parturient himenaeos dictum parturient parturient lacinia parturient sociosqu nascetur cras suspendisse inceptos...</div>
+								<br>
+								<div><a class="mt-md" href="demo-restaurant-press-detail.html">Read More <i class="fa fa-long-arrow-right"></i></a></div>
+							</div>
+						</div>
+						</section>
+						<section>
+							
+											<div class="row">
+												<div class="col-sm-12">
+													<div class="serp-result-info">
+														1 - 15 dari 372.941 Hasil                
+													</div>
+												</div>
+												
+												<div class="col-sm-12">
+													
+													<div class="serp-paging">
+														<ul id="serp-pagination-link" class="pagination">
+															<li style="display:none"><span>1 <span class="sr-only">(current)</span>sebelumnya</span>
+															</li>
+															<li class="active"><span>1 <span class="sr-only">(current)</span></span>
+															</li>
+															<li>
+																<a href="/rumah-dijual/page-2" class="ajax">2</a>
+															</li>
+															<li>
+																<a href="/rumah-dijual/page-3" class="ajax">3</a>
+															</li>
+															<li>
+																<a href="/rumah-dijual/page-4" class="ajax">4</a>
+															</li>
+															
+															<li>
+																<a class="ajax" href="/rumah-dijual/page-2">Selanjutnya</a>
+															</li>
+														</ul>
+													</div>
+												</div>
+											</div>
+										
 						</section>
 						</div>
 						</div>
