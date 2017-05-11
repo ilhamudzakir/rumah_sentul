11b52c659ba781a34b3efbf8e55cd215b94318d2
algam <algamonline@gmail.com>
2017-05-11 11:39:41 +0700

    side bar front

--- application/views/sidebar_front.php
+++ application/views/sidebar_front.php
@@ -13,17 +13,19 @@
 
 									<div class="row">
 										<div class="col-sm-6">
-											<a  href="">
+											<a  href="<?php echo base_url()."news/detail/".$key->id."/".url_title($key->title) ?>">
 												<img src="<?php echo base_url() ?>assets/uploads/news/<?php echo $key->id?>/<?php echo $key->images ?>" width="100%">
 											</a>
 										</div>
 										<div class="col-sm-6">
-										<a class="wd" href="">
+										<a class="wd" href="<?php echo base_url()."news/detail/".$key->id."/".url_title($key->title) ?>">
 										<?php echo $key->title ?>
 											</a>
 										</div>
 									</div>
-									<?php } ?>			
+									<?php } ?>
+
+
 									<div class="row">
 										<div class="col-md-12 page-fb">
 											<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FPT-Flip-Flops-Solution-623119867764470%2F&tabs=timeline&width=340&height=417&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=254507101579491" width="100%" height="370px" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
